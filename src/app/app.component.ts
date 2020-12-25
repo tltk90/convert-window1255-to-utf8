8d255cba3de8bce9de558f30e562cd63748e3740
Tzahi Levi <thetzahilevi@gmail.com>
2020-12-25 09:23:58 +0200

    first modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'window1255-to-utf8-converter';
+  title = 'window1255 to utf8 converter';
 }
